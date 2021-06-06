7fd34a8e120469ea1ba4b25b349c8efef940663a
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2021-06-07 00:00:26 +0530

    Learned about varibles,alert,prompt In JS..

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Hello world,this is lavish");
+console.log("34"+19);
+//3419
+console.log(10-"3");
+//Javscript think that user might want to put the no,instead 
+// of 3,=-> Output 7
+
+console.log("hello"-"bye");
+//NaN->Not a number
+
+console.log(5>10);
+//false
+
+console.log(5===5);
+//true
+//In js to check whether two things are equal or not
+//use ===
+
+console.log(3!=3);
+//false
+
+console.log(4!==5);
+//true
+
+console.log(false+false);
+//0
+
+console.log(false+true);
+//1
---
+++ variables.js
@@ -0,0 +1,39 @@
+//variable in javascript can starts with a letter
+//and can end with a number
+//var firstName=prompt("Enter First No");
+//var secondName=prompt("Enter 2nd no");
+//var sum=Number(firstName)+Number(secondName);
+//console.log("The sum is"+sum);
+console.log(firstName);
+//alert("Sum is "+sum);
+
+//alert("hi there");
+
+
+//prompt() is a method of the Global window object 
+//found in browsers. It's not part of the JavaScript 
+//language. If you don't run your code in a browser or 
+//an environment that supports the Browser Object Model,
+// window won't be there and if window isn't there, neither 
+//will prompt() be. Run your code in a browser.
+
+var a=5;
+console.log(a);
+//we can reassign a to another type of 
+//entry
+//if we do not assign any thing to the
+//variable,then we get the value of that
+//variable in console as undefined
+a="hello";
+console.log(a);34
+
+
+var firstName="Sussy";
+if(firstName==="Billy")
+{
+    alert("Hi Billy");
+}
+else if(firstName=="Sussy")
+{
+    alert("Hi sussi");
+}
